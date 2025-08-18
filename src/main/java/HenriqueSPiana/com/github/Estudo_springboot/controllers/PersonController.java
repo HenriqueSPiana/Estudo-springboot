fa8f2034e090525625f856051b127df46c01b96b
HenriqueSPiana <103382089+HenriqueSPiana@users.noreply.github.com>
2025-08-18 17:41:10 -0300

    corrige warns e utiliza outro metodos para controlers de crud, facilitando leitura

--- src/main/java/HenriqueSPiana/com/github/Estudo_springboot/controllers/PersonController.java
+++ src/main/java/HenriqueSPiana/com/github/Estudo_springboot/controllers/PersonController.java
@@ -3,6 +3,7 @@ import HenriqueSPiana.com.github.Estudo_springboot.services.PersonServices;
 import HenriqueSPiana.com.github.Estudo_springboot.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public class PersonController {
     @Autowired
     private PersonServices service;
     //GET http://localhost:8080/person/{id}
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findByid(@PathVariable("id") Long id){
         Person personId = service.findByID(id);
         return personId;
@@ -22,14 +23,14 @@ public class PersonController {
 
 
     //GET http://localhost:8080/person
-    @RequestMapping(method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll(){
         List<Person> personId = service.findAll();
         return personId;
     }
 
     //POST http://localhost:8080/person
-    @RequestMapping(method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     public Person create(@RequestBody Person person){
 
        Person personId = service.create(person);
@@ -37,7 +38,7 @@ public class PersonController {
     }
 
     //PUT http://localhost:8080/person
-    @RequestMapping(method = RequestMethod.PUT,consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     public Person update(@RequestBody Person person){
        Person personId = service.update(person);
         return personId;
@@ -45,9 +46,10 @@ public class PersonController {
 
 
     //DELETE http://localhost:8080/person
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable("id") Long id){
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id){
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
