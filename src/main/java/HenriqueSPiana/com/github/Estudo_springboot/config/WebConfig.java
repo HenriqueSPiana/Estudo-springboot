1f36e4f238521a3e851585c26651d0fe527f757f
HenriqueSPiana <103382089+HenriqueSPiana@users.noreply.github.com>
2025-09-12 17:22:35 -0300

    Implementa content negotiation via HEADER parameter

--- src/main/java/HenriqueSPiana/com/github/Estudo_springboot/config/WebConfig.java
+++ src/main/java/HenriqueSPiana/com/github/Estudo_springboot/config/WebConfig.java
@@ -12,10 +12,26 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        /**
+         *
+         * VIA QUERY PARAM http://localhost:8080/api/person/v1/2?mediaType=xml
+         *
+         *
         WebMvcConfigurer.super.configureContentNegotiation(configurer);
         configurer.favorParameter(true)
                 .parameterName("mediaType")
                 .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json",MediaType.APPLICATION_JSON)
+                .mediaType("XML",MediaType.APPLICATION_XML);
+       */
+        //VIA HEADER PARAM http://localhost:8080/api/person/v1/2?mediaType=xml
+        //PARA USAR VIA HEADER NECESSARIO UTILIZAR O PARAMETRO Accept DE VALOR = application/xml
+        WebMvcConfigurer.super.configureContentNegotiation(configurer);
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json",MediaType.APPLICATION_JSON)
